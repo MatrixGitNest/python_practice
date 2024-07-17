57d98684e0059bf79863c71637c96d463acaa624
MatrixGitNest <matrix221b@outlook.com>
2024-07-17 21:04:40 +0530

    initial commit

---
+++ lists.py
@@ -0,0 +1,29 @@
+#prints all the values in the list
+courses=['car','bike','plane','ship','ATV','tank']
+print(courses)
+
+#prints the value from the index 0 & 1
+courses=['car','bike','plane','ship','ATV','tank']
+print(courses[0:2])
+
+#prints all the values starting from index 2 
+courses=['car','bike','plane','ship','ATV','tank']
+print(courses[2:])
+
+#negative indexes
+courses=['car','bike','plane','ship','ATV','tank']
+print(courses[1:-1]) 
+
+#multiple negative indexes
+courses=['car','bike','plane','ship','ATV','tank']
+print(courses[-7:-1])    
+
+
+#  step indexes
+courses=['car','bike','plane','ship','ATV','tank','cyclce','rover']
+print(courses[1:9:3])    
+
+
+#print all from 1 
+courses=['car','bike','plane','ship','ATV','tank']
+print(courses)    
